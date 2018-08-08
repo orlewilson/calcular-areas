f2a62dadf82726edf0f4edc7a386b3d2ab83b33d
orlewilson maia <orlewilson.maia@gmail.com>
2018-08-08 16:31:43 -0400

    criacao do arquivo principal

---
+++ calcular-area.c
@@ -0,0 +1,17 @@
+/*
+	Oficina Git
+	Professor: Orlewilson B. Maia
+	Equipe:
+	Data: 08/08/2018
+	Descricao:	Calcular area de 
+				circulo, triangulo 
+				equilatero e quadrado
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello World");
+	return 0;
+}
